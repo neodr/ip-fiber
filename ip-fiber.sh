a45e9c83254b6db6a6efbe251bb9fbf8c780d95c
neodr <neodr@onet.pl>
2018-03-09 16:17:57 +0100

    Correct ip-fiber

--- ip-fiber.sh
+++ ip-fiber.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+
 echo "change setting network card"
-ip address add 192.168.113.32 dev eth3
-ip route add 192.168.113.0/24 dev eth3
+set -x
 
-ip address add 192.168.114.32 dev eth2
-ip route add 192.168.114.0/24 dev eth2
+function config_one_card() {
+	ip="$1"
+	ip2="$2"
+	dev="$3"	
+	ip addr flush dev $dev
+	ip address add $ip dev $dev
+	ip route add $ip2/24 dev $dev
+	ifconfig $dev mtu 9000
 
-ifconfig eth2 mtu 9000
-ifconfig eth3 mtu 9000
+}
+
+config_one_card 192.168.113.200 192.168.113.0 ens11f0
+config_one_card 192.168.114.200 192.168.114.0 ens11f1
 # settings for network card (IRQ affinity) - used e.g. in: https://github.com/yedinocommunity/galaxy42/wiki/Bench
